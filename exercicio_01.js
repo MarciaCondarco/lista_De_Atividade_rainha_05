aa3d89656daf00e2501feabebf02b564780edb91
Marcy_ <142843705+MarciaCondarco@users.noreply.github.com>
2024-09-09 18:33:51 -0300

    Add files via upload

---
+++ exercicio_01.js
@@ -0,0 +1,12 @@
+resultado = 0 
+
+while(true){
+    numero_usuario=prompt("digite um numero:")
+    
+    if (numero_usuario==0){
+        break
+    }
+    resultado = resultado + parseInt(numero_usuario)
+    }
+    alert("a soma de todos os numeros é: " + resultado)
+    alert('testejyuy')
